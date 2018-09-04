5799088bb4bcef9c58d101d0a8f715163a9da580
PawelLukaszewski <pawel.t.lukaszewski@gmail.com>
2018-09-04 19:47:11 +0200

    Initial commit

--- src/main/java/pl/pawellukaszewski/controllers/MainController.java
+++ src/main/java/pl/pawellukaszewski/controllers/MainController.java
@@ -1,12 +1,23 @@
 package pl.pawellukaszewski.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import pl.pawellukaszewski.models.SocketConnector;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    @FXML
+    Button buttonSend;
+
+    @FXML
+    TextArea textMessages;
+
+    @FXML
+    TextField textWriteMessage;
 
     private SocketConnector socketConnector = SocketConnector.getInstance();
 
